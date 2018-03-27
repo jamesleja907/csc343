2a96f302c741c58ca2ee4d6ba475f9705ba66e5c
James Leja <james.leja@mail.utoronto.ca>
2018-03-27 12:43:08 -0400

    Comments to queries

--- A3/q3.sql
+++ A3/q3.sql
@@ -4,7 +4,7 @@ drop table if exists q3 cascade;
 
 create table q3(name VARCHAR(50));
 
---Get all car ids rented in Toronto in 2017
+-- Get all car ids rented in Toronto in 2017
 create view TO_seventeen as
   select reservation.id as res_id, reservation.car_id, car.model_id
   from Reservation join Car on reservation.car_id = car.id
@@ -13,23 +13,26 @@ create view TO_seventeen as
    and extract(year from to_date) = 2017
    and reservation.status = 'Completed';
 
---Get the number of model
+-- Get the number of models rented in Toronto in 2017
 create view num_models as
   select model_id, count(*)
   from TO_seventeen
   group by model_id;
 
+-- Get the names of the models rented
 create view popular_models as
   select model.name, count
   from num_models join model on num_models.model_id = model.id;
 
+-- rank the models rented in toronto by # times rented, breaking ties by name
 create view model_ranks as
   select name, count, row_number() over (order by count desc, name) as rank
   from popular_models;
 
+-- get the most popular model rented in Toronto in 2017
 create view result3 as
   select name
   from model_ranks
   where rank = 1;
 
-insert into q3 select * from result3;
+--insert into q3 select * from result3;
